c7e4f677ad7138847e5fb25960b0504468bf3a36
helium18 <helium.dev@tuta.io>
2022-02-11 08:21:01 +0530

    Add animation
    Add a gradient Background Animation
    
    Updated the tailwind config to and the image tag in html to reflect the same.

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,49 @@ module.exports = {
             black: "#414868",
             d_black: "#24283b",
         },
+
+        extend: {
+            animation: {
+                "gradient-x": "gradient-x 10s ease infinite",
+                "gradient-y": "gradient-y 10s ease infinite",
+                "gradient-xy": "gradient-xy 10s ease infinite",
+            },
+            keyframes: {
+                "gradient-y": {
+                    "0%, 100%": {
+                        "background-size": "400% 400%",
+                        "background-position": "center top",
+                    },
+                    "50%": {
+                        "background-size": "200% 200%",
+                        "background-position": "center center",
+                    },
+                },
+                "gradient-x": {
+                    "0%, 100%": {
+                        "background-size": "200% 200%",
+                        "background-position": "left center",
+                    },
+                    "50%": {
+                        "background-size": "200% 200%",
+                        "background-position": "right center",
+                    },
+                },
+                "gradient-xy": {
+                    "0%, 100%": {
+                        "background-size": "400% 400%",
+                        "background-position": "center center",
+                    },
+                    "50%": {
+                        "background-size": "200% 200%",
+                        "background-position": "right center",
+                    },
+                    "25%": {
+                        "background-size": "200% 200%",
+                        "background-position": "left center",
+                    },
+                },
+            },
+        },
     },
-    plugins: [],
 };
